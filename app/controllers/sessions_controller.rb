97cb45fdfdd7a16e1a253947f9f79ec1a412721f
sotirelisc <gtakriz@hotmail.com>
2016-06-02 07:59:34 +0000

    Test sign-in controller in production.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < Devise::SessionsController
-  def new
-    if request.subdomain.blank? || request.subdomain == "www"
-      flash[:error] = "Access restricted!"
-      redirect_to :root
-    else
-      super
-    end
-  end
+  # def new
+  #   if request.subdomain.blank? || request.subdomain == "www"
+  #     flash[:error] = "Access restricted!"
+  #     redirect_to :root
+  #   else
+  #     super
+  #   end
+  # end
 end
