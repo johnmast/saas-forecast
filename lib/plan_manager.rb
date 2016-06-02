a677c74ef125a1209bac3b85f074d5bde95e93e8
sotirelisc <gtakriz@hotmail.com>
2016-06-02 18:02:40 +0000

    PlanManager issues trying to fix.

--- lib/plan_manager.rb
+++ lib/plan_manager.rb
@@ -1,56 +1,68 @@
 module PlanManager
-  
-  def current_plan
-    account = current_account
-    plan_id = account.stripe_plan_id
-    if plan_id.nil?
-      return nil
-    end
-    plan = Plan.find_by_stripe_id plan_id
-  end
+    #Get the current plan of the user
+    def current_plan
+        account = current_account
 
-  def current_account
-    Account.find_by_email(current_user.email)
-  end
+        plan_id = account.stripe_plan_id
 
-  def plan_check (action, subject)
-    if current_plan.nil?
-      check_no_plan_restrictions(action, subject)
-    elsif current_plan.stripe_id == "free-plan"
-      check_free_plan_restrictions(action, subject)
-    else
-      check_premium_plan_restrictions(action, subject)
-    end
-  end
-
-  def check_no_plan_restrictions (action, subject)
-    if action == "create"
-      if subject == "weather_grid"
-        return "You need a plan to create a WeatherGrid. Please visit <a href='/subscriptions'>Plans</a> to buy a plan!"
-      end
-    end
-  end
-
-  def check_free_plan_restrictions(action, subject)
-    if action == "create"
-      if subject == "weather_grid"
-        count = WeatherGrid.count
-        if count >= 2
-          return "Your plan only allows 2 WeatherGrids. Please visit <a href='/subscriptions'>Plans</a> to buy a plan!"
+        if plan_id.nil?
+            return nil
         end
-      end
-    end
-  end
 
-  def check_premium_plan_restrictions(action, subject)
-    if action == "create"
-      if subject == "weather_grid"
-        count = WeatherGrid.count
-        if count >= 6
-          return "You need a plan to create WeatherGrid. Please visit <a href='/subscriptions'>Plans</a> to buy a plan!"
-        end
-      end
+        plan = Plan.find_by_stripe_id (plan_id)
+
     end
-  end
-  
+
+
+    def current_account
+
+        Account.find_by_email(current_user.email)
+
+    end
+
+    #Check whether user's plan allows current action
+    def plan_check (action, subject)
+        if current_plan.nil?
+            check_no_plan_restrictions(action, subject)
+        elsif current_plan.stripe_id == "free-plan"
+            check_free_plan_restrictions(action, subject)
+        else
+            check_awesome_plan_restrictions(action, subject)
+        end
+
+    end
+
+    def check_no_plan_restrictions (action, subject)
+        if action == "create"
+            if subject == "weather_grid"
+                return "You need a plan to create WeatherGrid. Please visit <a href='/subscriptions'>Plans</a> to buy a plan"
+            end
+        end
+    end
+
+    def check_free_plan_restrictions(action, subject)
+        if action == "create"
+            if subject == "weather_grid"
+                count = WeatherGrid.count
+
+                if count >= 2
+                    return "Your plan only allows 2 WeatherGrids. Please visit <a href='/subscriptions'>Plans</a> to buy a plan"
+                end
+                
+            end
+        end
+
+    end
+
+    def check_awesome_plan_restrictions (action, subject)
+        if action == "create"
+            if subject == "weather_grid"
+                count = WeatherGrid.count
+
+                if count >= 6
+                    return "You need a plan to create WeatherGrid. Please visit <a href='/subscriptions'>Plans</a> to buy a plan"
+                end
+            end
+        end
+    end
 end
