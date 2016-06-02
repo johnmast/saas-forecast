9f0d1f13b0c09999d8e041241807c96ab2e48169
sotirelisc <gtakriz@hotmail.com>
2016-06-02 17:54:41 +0000

    Changes to registrations controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,12 +10,12 @@ class RegistrationsController < Devise::RegistrationsController
   #   end
   # end
   
-  # protected
-  #   def after_sign_up_path_for(resource)
-  #     new_user_session_url(subdomain: resource.subdomain)
-  #   end
+  protected
+    def after_sign_up_path_for(resource)
+      new_user_session_url(subdomain: resource.subdomain)
+    end
     
-  #   def after_inactive_sign_up_path_for(resource)
-  #     new_user_session_url(subdomain: resource.subdomain)
-  #   end
+    def after_inactive_sign_up_path_for(resource)
+      new_user_session_url(subdomain: resource.subdomain)
+    end
 end
